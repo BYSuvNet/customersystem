79f8c14c9d21a56dd81f27306a592e0a3f581a9f
Gus <gusgit@fastmail.org>
2025-12-04 14:35:39 +0100

    Refactor menu to use ArrowMenu

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ using CustomerSystem.UI;
 using CustomerSystem.UI.Views;
 
 ICustomerRepository customerRepository = new InMemoryCustomerRepository();
+// ICustomerRepository customerRepository = new DapperCustomerRepository("minConnectionString");
 
 IView[] views = [new AddCustomerView(), new ListAllCustomersView(customerRepository), new RemoveCustomerView(customerRepository)];
 string[] menuOptions = ["Lägg till kund", "Visa alla kunder", "Ta bort kund"];
 
 while (true)
 {
-    int choice = Menu.Show("Huvudmeny", menuOptions);
+    int choice = ArrowMenu.Show("Huvudmeny", menuOptions);
 
     if (choice == -1) break;
 
