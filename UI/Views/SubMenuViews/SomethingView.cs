29c47aaacba582d99a3b180f9a2b8d5ca92e14a4
Gus <gusgit@fastmail.org>
2025-12-11 14:28:20 +0100

    Add prompt to return

--- UI/Views/SubMenuViews/SomethingView.cs
+++ UI/Views/SubMenuViews/SomethingView.cs
@@ -8,6 +8,8 @@ public class SomethingView : IView
         Här kan vi göra vadsomhelst. Klassen tar inte in något i sin konstruktor,
         men skulle kunna ta emot vad som helst den behöver för att göra sin grej.
         Kanske en service, ett repository eller liknande.
+
+        Tryck på valfri knapp för att gå tillbaka.
         """;
 
     public void Show()
