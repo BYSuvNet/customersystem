c6a27c062913c7ad1723621bfde9c2c3dc57c19c
Gus <gusgit@fastmail.org>
2025-12-11 14:27:40 +0100

    Enhance ArrowMenu to support an optional exit option and improve selection handling

--- UI/ArrowMenu.cs
+++ UI/ArrowMenu.cs
@@ -2,30 +2,29 @@ namespace CustomerSystem.UI;
 
 public static class ArrowMenu
 {
-    public static int Show(string title, string[] options)
+    public static int Show(string title, string[] options, string? exitOption = null)
     {
         int currentSelection = 0;
         Console.CursorVisible = false;
 
+        int totalOptions = options.Length + (string.IsNullOrEmpty(exitOption) ? 0 : 1);
+
         while (true)
         {
             Console.Clear();
+            Console.WriteLine($"[ {title} ]");
 
-            Console.WriteLine($"-- {title} --");
-
-            for (int i = 0; i < options.Length; i++)
+            for (int i = 0; i < totalOptions; i++)
             {
+                string optionText = i < options.Length ? options[i] : exitOption!;
                 if (i == currentSelection)
                 {
                     Console.BackgroundColor = ConsoleColor.Gray;
                     Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"> {options[i]}");
+                    Console.WriteLine($"> {optionText}");
                     Console.ResetColor();
                 }
-                else
-                {
-                    Console.WriteLine($"  {options[i]}");
-                }
+                else Console.WriteLine($"  {optionText}");
             }
 
             ConsoleKey key = Console.ReadKey(true).Key;
@@ -33,13 +32,14 @@ public static class ArrowMenu
             switch (key)
             {
                 case ConsoleKey.UpArrow:
-                    currentSelection = (currentSelection - 1 + options.Length) % options.Length;
+                    currentSelection = (currentSelection - 1 + totalOptions) % totalOptions;
                     break;
                 case ConsoleKey.DownArrow:
-                    currentSelection = (currentSelection + 1) % options.Length;
+                    currentSelection = (currentSelection + 1) % totalOptions;
                     break;
                 case ConsoleKey.Enter:
-                    return currentSelection;
+                    if (currentSelection == options.Length) return -1;
+                    else return currentSelection;
                 case ConsoleKey.Escape:
                     return -1;
             }
