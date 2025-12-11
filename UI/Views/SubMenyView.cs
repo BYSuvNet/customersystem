bd05c45e104ecac21b276aefe10ab04ba71bf70f
Gus <gusgit@fastmail.org>
2025-12-11 14:10:31 +0100

    Fix formatting and improve user prompt in SubMenuView

--- UI/Views/SubMenyView.cs
+++ UI/Views/SubMenyView.cs
@@ -6,14 +6,14 @@ public class SubMenuView : IView
 {
     public string Title => "-- En undermeny --";
 
-    public void Show()
-    {
-        IView[] views = [new SomethingView(),
+    IView[] views = [new SomethingView(),
                  new SomethingElseView()];
 
+    public void Show()
+    {
         while (true)
         {
-            int choice = ArrowMenu.Show("Gör ditt val:", views.TitlesToStringArray()); //En extension method, se EntensionMethods.cs
+            int choice = ArrowMenu.Show("Undermeny: ", views.TitlesToStringArray()); //En extension method, se EntensionMethods.cs
 
             if (choice == -1) break; //Break kommer att avbryta show och därmed återgå till den tidigare vyn
 
